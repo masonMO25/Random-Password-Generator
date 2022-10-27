9878a4757ec665880d89727f093d833043293069
masonMO25 <masonneale@gmail.com>
2022-10-27 16:08:12 -0500

    added arrays and pw length promt.

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,36 @@
+// need prompt for length between 8-128
+// need prompt for uppercase, lowercase, numeric, special characters
+// generates password that matches criteria
 // Assignment code here
+
+//Arrays
+let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+let lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+function generatePassword () {
+  const confirmLength = prompt("How many characters would you like your password to be?");
+  while(confirmLength <= 7 || confirmLength >= 129) {
+    alert("Password length must be between 8-128 characters Try again");
+    const confirmLength = (prompt("How many characters would you like your password to contain?"));
+}
+    alert(`Your password will have ${confirmLength} characters`);
+}
+
+
+
+
+
+
+
+
+
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
